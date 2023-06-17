6e2c7cf5b38a491a09f0289dd427b926cdce2892
signo <you@example.com>
2023-06-17 19:16:19 +0200

    play buttons created

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -3,19 +3,15 @@ import './css/Selection.css'
 function Selection() {
 
     return (
-        <body style={{ backgroundColor: 'green' }}>
-            <div id="c1">
-                <h1 id="c2">Il Mio Gioco</h1>
-                <div className="score">
-                    Punteggio: <span id="score">0</span>
-                </div>
-                <Link to="/gamex">
-                    <button className="play-button">Gioca</button>
-                </Link>
-            </div>
-
-        </body>
-
+        <div id="c1">
+            <h1 id="c2">The Tris Game</h1>
+            <Link to="/gamex">
+                <button className="play-button">ONline</button>
+            </Link>
+            <Link to="/gamex">
+                <button className="play-button">OFFline</button>
+            </Link>
+        </div>
     )
 }
 export default Selection
