7933e4ccb7a6d5a8954f546978b8ea761a274828
signo <you@example.com>
2023-06-22 16:50:22 +0200

    session info

--- src/components/waiting.jsx
+++ src/components/waiting.jsx
@@ -1,6 +1,56 @@
 import React from "react";
 import './css/Waiting.css'
+import { useEffect } from "react";
+
 function Waiting() {
+    async function getSession() {
+        try {
+            const response = await fetch("/api/session/getFree", {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+
+            const result = await response.json();
+            return result;
+        }
+        catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    async function createSession() {
+        try {
+            const response = await fetch("/api/session/create", {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+            const result = await response.json();
+            return result;
+        }
+        catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    useEffect(() => {
+        async function getFreeSession() {
+            const session = await getSession();
+            if (session != "") {
+                window.location.href = "/online";
+            }
+            else {
+                await createSession();
+            }
+        }
+        getFreeSession();
+    }, []);
+
     return (
         <div className="center">
             <h1>Waiting for opponent...</h1>
