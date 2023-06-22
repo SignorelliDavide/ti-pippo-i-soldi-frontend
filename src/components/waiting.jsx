c5e8415126847b086c74232145657e6c489170cf
Signo <signorelli.davide.studente@itispaleocapa.it>
2023-06-22 21:04:01 +0200

    create modified

--- src/components/waiting.jsx
+++ src/components/waiting.jsx
@@ -1,60 +1,44 @@
-import React from "react";
 import './css/Waiting.css'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+async function getSession() {
+    try {
+        const response = await fetch("/api/session/getFree", {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
+
+        const result = await response.json();
+        return result;
+    }
+    catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+async function createSession() {
+    console.log("createSession");
+    try {
+        const response = await fetch("/api/session/create", {
+            method: "POST",
+            credentials: "include",
+        });
+        const result = await response.json();
+        console.log("Success:", result);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
 
 function Waiting() {
-    async function getSession() {
-        try {
-            const response = await fetch("/api/session/getFree", {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            });
-
-            const result = await response.json();
-            return result;
-        }
-        catch (error) {
-            console.error("Error:", error);
-        }
-    }
-
-    async function createSession() {
-        try {
-            const response = await fetch("/api/session/create", {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            });
-            const result = await response.json();
-            return result;
-        }
-        catch (error) {
-            console.error("Error:", error);
-        }
-    }
-
-    useEffect(() => {
-        async function getFreeSession() {
-            const session = await getSession();
-            if (session != "") {
-                window.location.href = "/online";
-            }
-            else {
-                await createSession();
-            }
-        }
-        getFreeSession();
-    }, []);
-
+    /*useEffect(() => { createSession() });*/
     return (
         <div className="center">
             <h1>Waiting for opponent...</h1>
-            <div class="loader">
+            <div className="loader">
                 <span></span>
                 <span></span>
                 <span></span>
@@ -63,4 +47,5 @@ function Waiting() {
         </div>
     );
 }
+
 export default Waiting;
